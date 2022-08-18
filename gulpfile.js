23ca503b9443ecda39b231bbff093a2b04b3eac5
Javivy <84439052+Javivy@users.noreply.github.com>
2022-08-18 17:09:01 -0400

    Pull styles from branch 'build-headline'

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-import { task, src, dest } from 'gulp';
-import autoprefixer from 'gulp-autoprefixer';
-
-task('styles', ()=>{
-    src('styles/styles.css')
-        .pipe(autoprefixer())
-        .pipe(dest('build'));
-});
