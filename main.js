5f69ecfcb655ff4a69f4e6a06e4fef07cbede4d4
Javivy <84439052+Javivy@users.noreply.github.com>
2022-08-24 18:32:19 -0400

    Create and style responsive web design for desktop

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let width = window.innerWidth;
+let full_name = document.getElementById('full-name');
+let fst_name = document.getElementById('first-name');
+let lst_name = document.getElementById('last-name');
+
+if(width >= 1024) {
+    full_name.required = false;
+} else if (width < 1024) {
+    full_name.required = true;
+    fst_name.required = false;
+    lst_name.required = false;
+}
