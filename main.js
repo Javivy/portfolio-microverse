4d497bec527cf5879fca90d38cbf37c804147b7f
Javivy <84439052+Javivy@users.noreply.github.com>
2022-08-25 14:04:24 -0400

    Make changes on 'About me' section and 'Form' section

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ let full_name = document.getElementById('full-name');
 let fst_name = document.getElementById('first-name');
 let lst_name = document.getElementById('last-name');
 
-if(width >= 1024) {
-    full_name.required = false;
-} else if (width < 1024) {
-    full_name.required = true;
-    fst_name.required = false;
-    lst_name.required = false;
-}
+// if(width >= 1024) {
+//     full_name.required = false;
+// } else if (width < 1024) {
+//     full_name.required = true;
+//     fst_name.required = false;
+//     lst_name.required = false;
+// }
+
+fst_name.required = false;
+lst_name.required = false;
