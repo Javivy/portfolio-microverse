af57c2c33e7f45895270dc46b79302bfbb6b44a0
Javivy <84439052+Javivy@users.noreply.github.com>
2022-09-05 19:37:27 -0400

    Fix mobile-ul to don't appear when screen is wider

--- main.js
+++ main.js
@@ -2,16 +2,13 @@ const openMenuBtn = document.querySelector('.open-menu-btn');
 const closeMenuBtn = document.querySelector('.close-menu-btn');
 const mobileMenu = document.querySelector('.visible');
 const mobileNav = document.querySelector('.mobile-ul');
-const width = document.innerWidth();
 
-if (width < 768) {
-  openMenuBtn.addEventListener('click', () => {
-    mobileMenu.style.display = 'block';
-    mobileNav.style.display = 'none';
-  });
+openMenuBtn.addEventListener('click', () => {
+  mobileMenu.style.display = 'block';
+  mobileNav.style.zIndex = '-1';
+});
 
-  closeMenuBtn.addEventListener('click', () => {
-    mobileMenu.style.display = 'none';
-    mobileNav.style.display = 'flex';
-  });
-}
+closeMenuBtn.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+  mobileNav.style.zIndex = '1';
+});
