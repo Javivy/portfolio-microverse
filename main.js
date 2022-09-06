8a8de7b71ba2f1018e09747bf92d726ec1258328
Duncan Njau G <102324214+Duncannjau@users.noreply.github.com>
2022-09-06 23:53:06 +0300

    Add js function for popup

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const openMenuBtn = document.querySelector('.open-menu-btn');
 const closeMenuBtn = document.querySelector('.close-menu-btn');
 const mobileMenu = document.querySelector('.visible');
 const mobileNav = document.querySelector('.mobile-ul');
+const openProject = () => {
+  projectPopUp.style.display = 'block';
+}
 
 const disableScroll = () => {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
