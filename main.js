b18b37cc7478eb011b0fd161e9452555651ff3ca
Javivy <84439052+Javivy@users.noreply.github.com>
2022-09-06 12:46:03 -0400

    Fix and make required changes for the review about the mobile-menu

--- main.js
+++ main.js
@@ -3,12 +3,38 @@ const closeMenuBtn = document.querySelector('.close-menu-btn');
 const mobileMenu = document.querySelector('.visible');
 const mobileNav = document.querySelector('.mobile-ul');
 
+const disableScroll = () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  window.onscroll = () => {
+    window.scrollTo(scrollLeft, scrollTop);
+  };
+};
+
+const enableScroll = () => {
+  window.onscroll = () => {};
+};
+
 openMenuBtn.addEventListener('click', () => {
   mobileMenu.style.display = 'block';
+  disableScroll();
   mobileNav.style.zIndex = '-1';
 });
 
 closeMenuBtn.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
+  enableScroll();
   mobileNav.style.zIndex = '1';
 });
+
+// eslint-disable-next-line no-unused-vars
+const openMenu = () => {
+  mobileMenu.style.display = 'block';
+};
+
+// eslint-disable-next-line no-unused-vars
+const closeMenu = () => {
+  mobileMenu.style.display = 'none';
+  enableScroll();
+};
