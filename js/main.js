7ca1d4f36b98b88d974c1dd93ae4edcc879a5378
Javivy <84439052+Javivy@users.noreply.github.com>
2022-10-15 01:43:36 -0400

    Add leaderboard project to the portfolio

--- js/main.js
+++ js/main.js
@@ -35,8 +35,8 @@ sr.reveal(form, {
 
 const projectData = [
   {
-    name: 'Capstone Project',
-    title: 'Capstone Project',
+    name: 'Conference Project',
+    title: 'Conference Project, Game DEV Conference',
     description: "This is the capstone project for the 1st module at Microverse, the aim for this project is to show off and put into practice all the knowledge we've been learning throughout the 1st module.",
     mobileSnapshot: 'images/capstone-project-mobile.jpg',
     desktopSnapshot: 'images/Capstone-project.png',
@@ -46,14 +46,14 @@ const projectData = [
   },
 
   {
-    name: 'Multi-Post stories Gain+Glory',
-    title: 'Project 2',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    mobileSnapshot: 'images/capstone-project-mobile.jpg',
-    desktopSnapshot: 'images/Capstone-project.png',
-    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    linkLiveServer: '#',
-    linkSource: '#',
+    name: 'Leaderboard Project',
+    title: 'Leaderboard Project, Making use of API',
+    description: "This is a leaderboard project, making use of an API from Microverse, working with HTML, CSS, JavaScript, Webpack, Linters, etc... On this project I practiced the use of API's, fetch, async-await, promises. Go check the live version and give it a try!",
+    mobileSnapshot: 'images/leaderboard-mobile.jpg',
+    desktopSnapshot: 'images/leaderboard-desktop.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Webpack', "API's"],
+    linkLiveServer: 'https://javivy.github.io/leaderboard/dist',
+    linkSource: 'https://github.com/Javivy/leaderboard',
   },
 
   {
